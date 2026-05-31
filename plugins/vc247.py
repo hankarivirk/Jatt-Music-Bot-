9dac27068ff9dcb3fd573a6500215c5a39542eb1
hankarivirk <hushanpreetvirk@gmail.com>
2026-05-31 13:47:00 +0530

    Update vc247.py

--- plugins/vc247.py
+++ plugins/vc247.py
@@ -6,7 +6,6 @@ from pyrogram.types import Message
 import database as db
 from plugins.utils import is_dj_or_admin, maintenance_check
 
-
 def register(app: Client) -> None:
 
     @app.on_message(filters.command("247") & filters.group)
@@ -14,18 +13,19 @@ def register(app: Client) -> None:
     async def vc247_cmd(client: Client, message: Message):
         chat_id = message.chat.id
         if not await is_dj_or_admin(client, chat_id, message.from_user.id):
-            await message.reply("Only DJs and admins can toggle 24/7 mode.")
+            await message.reply("⚠️ **Aᴄᴄᴇss Dᴇɴɪᴇᴅ:** DJs/Admins only.")
             return
         current = await db.get_setting(chat_id, "vc247")
         new_val = not current
         await db.set_setting(chat_id, "vc247", new_val)
         if new_val:
             await message.reply(
-                "<b>24/7 Mode Enabled</b>\n"
-                "The bot will stay in the voice chat even when idle."
+                "⏤͟͟͞͞★ **24/7 Mᴏᴅᴇ :** `Eɴᴀʙʟᴇᴅ`\n"
+                "✧ Bᴏᴛ ᴡɪʟʟ sᴛᴀʏ ɪɴ VC ᴇᴠᴇɴ ᴡʜᴇɴ ɪᴅʟᴇ."
             )
         else:
             await message.reply(
-                "<b>24/7 Mode Disabled</b>\n"
-                "The bot will leave after 3 minutes of inactivity."
+                "⏤͟͟͞͞★ **24/7 Mᴏᴅᴇ :** `Dɪsᴀʙʟᴇᴅ`\n"
+                "✧ Bᴏᴛ ᴡɪʟʟ ʟᴇᴀᴠᴇ ᴀғᴛᴇʀ ɪɴᴀᴄᴛɪᴠɪᴛʏ."
             )
+            
